485b5578da20985710ac02ad67e7a59493e02050
Leonardo Giacobbe <leonardogiacobbe@Air-di-Leonardo.home-life.hub>
2024-05-24 17:12:45 +0200

    Converted updated project to version r2022a

--- FirstPart/Greenhouse_previousVersionSimulink/init.m
+++ FirstPart/Greenhouse_previousVersionSimulink/init.m
@@ -21,7 +21,7 @@ c_f = 1005;% [J/(Kg*K)]
 m_f1 = 1225 * (50*10*3); %[Kg]
 %and module 2
 m_f2 = 1225 * (10*10*4);
-%average thermal capacity of the mass of the air in the greenhouse modules,
+%average thermal cap  ir in the greenhouse modules,
 %1 and 2 respectively
 C_f1 = c_f * m_f1;
 C_f2 = c_f * m_f2; %[J/K]
@@ -46,11 +46,23 @@ load('ExtractedData.mat');
 %(indeed it's given in hours, but SI time unit is second s). Transpose
 %since we want a row vector, while it's a column vector.
 timeseriesextK = timeseries(extK', time.*(60*60)');
-timeseriestrackK = timeseries(trackK', time.*(60*60)');
+timeseriestrack_m1K = timeseries(track_m1K', time.*(60*60)');
 timeseriesradiation = timeseries(radiation'*0.95, time.*(60*60)');
+timeseriestrack_m2K = timeseries(track_m2K', time.*(60*60)');
+
 
 % Initial theta_a
-theta0_a = 100;
+theta0_a = 291;
 
 % Sample time
 Ts = 1;%[s]
+
+%% Point 3a)
+q_star_m1 = 25000*4; %[W]
+q_star_m2 = 15000*4; %[W]
+
+%% Point 3b)
+qMAX_m1 = 25000*4;
+qmin_m1 = -25000*4;
+qMAX_m2 = 15000*4;  
+qmin_m2 = -15000*4;
