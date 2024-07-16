7fee077a11a9e55b574e0af02338898b26db5edd
Lorenzo Giampietro <lorenzo.giampietro68@gmail.com>
2024-07-16 11:03:58 +0200

    Aaron mi fa innervosire

--- ThirdPart/schedulingDB.m
+++ ThirdPart/schedulingDB.m
@@ -1,4 +1,31 @@
-function scheduling()
+function schedulingDB()
+    % Aggiungi il driver JDBC al java class path di Matlab
+    javaaddpath('C:\Users\loren\sqljdbc_12.6.3.0_ita\sqljdbc_12.6\ita\jars\mssql-jdbc-12.6.3.jre8.jar')
+    
+    % Configurazione della connessione al database
+    driver = 'com.microsoft.sqlserver.jdbc.SQLServerDriver';
+    serverName = 'LOLLOASUS'; 
+    portNumber = '1433';
+    databaseName = 'Scheduling';
+    
+    % URL di connessione
+    url = sprintf('jdbc:sqlserver://%s:%s;databaseName=%s;encrypt=false;', serverName, portNumber, databaseName);
+    
+    % Configurazione della connessione al database
+    conn = database(databaseName,'admin', 'password456', driver, url);
+    
+    % Verifica della connessione
+    if isopen(conn)
+        disp('Connessione al database riuscita')
+    else
+        disp('Connessione al database fallita')
+        disp(conn.Message);
+        return;
+    end
+
+    processing_times_gen = generate_pt(5);
+    disp(processing_times_gen);
+
     % Job processing times for each machine (10 jobs, 5 machines)
     processing_times = [
         5, 3, 6, 8, 4, 12, 12, 5, 3, 2;    % M1
@@ -112,3 +139,7 @@ function makespan = calculate_makespan(processing_times, sequence, machines)
     % Makespan is the last element in the completion times matrix
     makespan = completion_times(end, end);
 end
+
+% sqlquery = sprintf("INSERT INTO pt (id, machine_id, processing_time) VALUES (%d, %d, %d)", ...
+%            id, machine_id, processing_time);
+% execute(conn, sqlquery);
