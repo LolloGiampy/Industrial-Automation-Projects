859f6376e5bd964f472b2f622a3d833ba7448d1d
Aaron Kromer <akromer2000@gmail.com>
2024-07-16 11:34:16 +0200

    il viscido molesta sua fratello

--- ThirdPart/generate_pt.m
+++ ThirdPart/generate_pt.m
@@ -1,12 +1,11 @@
-function pts = generate_pt(num_jobs)
-    pts = zeros(6, num_jobs+1); 
-    pts(1,:) =0:num_jobs;
-    pts(:,1) =0:5;
-    for job = 2:(num_jobs+1)
+function generate_pt(num_jobs)
+    pts = zeros(6, num_jobs); 
+    pts(1,:) =1:num_jobs;
+    for job = 1:(num_jobs)
         for machine = 2:6
             processing_time = randi([1, 20]);
             pts(machine,job) = processing_time;
         end
     end
-    pts
-    pts_cutted=pts(2:end,2:end)
+  
+    pts.'
