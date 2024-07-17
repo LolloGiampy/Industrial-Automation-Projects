36256ad42aeb5c6a3b49acdd0c39f05aac781720
Aaron Kromer <akromer2000@gmail.com>
2024-07-17 18:21:24 +0200

    develop algorithm

--- ThirdPart/scheduling.m
+++ ThirdPart/scheduling.m
@@ -40,20 +40,28 @@ function scheduling()
     % Run the genetic algorithm
     [x, fval] = ga(fitnessFunction, nvars, [], [], Aeq, beq, lb, ub, [], ga_options);
 
-    % Extract job assignments, these variables contain the optimal sequences of jobs found by the genetic algorithm 
-    jobs_path1 = find(x(1:num_jobs) > 0.5);
-    jobs_path2 = find(x((num_jobs+1):end) > 0.5);
+    indices_path1 = find(x(1:num_jobs) > 0.5);
+    indices_path2 = find(x((num_jobs+1):end) > 0.5);
+    
+    % Estrai i valori corrispondenti a questi indici
+    values_path1 = x(indices_path1);
+    values_path2 = x(indices_path2);
+    
+    % Ordina i valori in ordine decrescente e mantieni gli indici
+    [~, sorted_indices1] = sort(values_path1, 'descend');
+    [~, sorted_indices2] = sort(values_path2, 'descend');
+    
+    % Ottieni gli indici originali ordinati
+    jobs_path1 = indices_path1(sorted_indices1);
+    jobs_path2 = indices_path2(sorted_indices2);
 
     % Display results
     disp(['Best sequence path 1: ', mat2str(jobs_path1)]);
     disp(['Best sequence path 2: ', mat2str(jobs_path2)]);
 
-    % Calculate makespans for the paths
-    makespan1 = calculate_makespan(processing_times, jobs_path1, [1, 3, 5]);
-    makespan2 = calculate_makespan(processing_times, jobs_path2, [2, 4, 5]);
 
     % Combined makespan
-    combined_makespan = max(makespan1, makespan2);
+    combined_makespan = calculate_makespan(processing_times, jobs_path1,jobs_path2, [1, 3, 5],[2, 4, 5]);
 
     disp(['Best combined makespan: ', num2str(combined_makespan)]);
 end
@@ -62,9 +70,22 @@ function combined_makespan = calculate_combined_makespan(x, processing_times)
     % Number of jobs
     num_jobs = size(processing_times, 2);
 
-    % Extract job assignments
-    jobs_path1 = find(x(1:num_jobs) > 0.5); % extract jobs assigned to the first path
-    jobs_path2 = find(x((num_jobs+1):end) > 0.5); % extract jobs assigned to the second path
+    indices_path1 = find(x(1:num_jobs) > 0.5);
+    indices_path2 = find(x((num_jobs+1):end) > 0.5);
+    
+    % Estrai i valori corrispondenti a questi indici
+    values_path1 = x(indices_path1);
+    values_path2 = x(indices_path2);
+    
+    % Ordina i valori in ordine decrescente e mantieni gli indici
+    [~, sorted_indices1] = sort(values_path1, 'descend');
+    [~, sorted_indices2] = sort(values_path2, 'descend');
+    
+    % Ottieni gli indici originali ordinati
+    jobs_path1 = indices_path1(sorted_indices1);
+    jobs_path2 = indices_path2(sorted_indices2);
+    
+
 
     % Check if any jobs are not assigned
     if isempty(jobs_path1) || isempty(jobs_path2)
@@ -72,43 +93,79 @@ function combined_makespan = calculate_combined_makespan(x, processing_times)
         return;
     end
 
-    % Calculate makespans for the path calling the function calculate_makespan
-    makespan1 = calculate_makespan(processing_times, jobs_path1, [1, 3, 5]);
-    makespan2 = calculate_makespan(processing_times, jobs_path2, [2, 4, 5]);
-
     % calculate combined makespan, it is determined by the path which takes longer
-    combined_makespan = max(makespan1, makespan2);
+    combined_makespan = calculate_makespan(processing_times, jobs_path1,jobs_path2, [1, 3, 5],[2, 4, 5]);
 end
 
-function makespan = calculate_makespan(processing_times, sequence, machines)
+function makespan = calculate_makespan(processing_times, sequence_path1,sequence_path2,machines_path1,machines_path2)
     % if the job sequence is empty, then the function immediately returns a makespan equal to zero
-    if isempty(sequence)
+    if isempty(sequence_path1)
         makespan = 0;
         return;
     end
-
+    if isempty(sequence_path2)
+        makespan = 0;
+        return;
+    end
     % variable initialization
-    num_jobs = length(sequence);
-    num_machines = length(machines);
-    completion_times = zeros(num_machines, num_jobs); % matrix which will be used to keep track of jobs' completion times 
+    num_jobs_path1 = length(sequence_path1);
+    num_jobs_path2 = length(sequence_path2);
+    completion_times_path1 = zeros(3, num_jobs_path1); % matrix which will be used to keep track of jobs' completion times 
 
     % Fill the completion times for the first job
-    completion_times(1, 1) = processing_times(machines(1), sequence(1)); % set completion time of the first job on the first machine
+    completion_times_path1(1, 1) = processing_times(machines_path1(1), sequence_path1(1)); % set completion time of the first job on the first machine
     % for each subsequent machine sum processing times, accumulating completion times
-    for m = 2:num_machines
-        completion_times(m, 1) = completion_times(m-1, 1) + processing_times(machines(m), sequence(1));
-    end
+    
+    completion_times_path1(2, 1) = completion_times_path1(1, 1) + processing_times(machines_path1(2), sequence_path1(1));
+    
+    queque_machine5=[zeros(num_jobs_path1+num_jobs_path2,2)];
+    queque_machine5(1,1) = completion_times_path1(2, 1);
+    queque_machine5(1,2) = processing_times(machines_path1(3), sequence_path1(1));
 
     % Fill the completion times for the rest of the jobs
-    for j = 2:num_jobs
+    for j = 2:num_jobs_path1
         % compute completion time on the first machine as the sum of completion time of the previous job and the current processing time
-        completion_times(1, j) = completion_times(1, j-1) + processing_times(machines(1), sequence(j));
+        completion_times_path1(1, j) = completion_times_path1(1, j-1) + processing_times(machines_path1(1), sequence_path1(j));
         % for each subsequent machine the completion time is the max between the completion time of the previous job on the same machine and the completion time of the current job on the previous machine
-        for m = 2:num_machines
-            completion_times(m, j) = max(completion_times(m-1, j), completion_times(m, j-1)) + processing_times(machines(m), sequence(j)); % then sum the current processing time with this max valuue
-        end
+        
+        completion_times_path1(2, j) = max(completion_times_path1(1, j), completion_times_path1(2, j-1)) + processing_times(machines_path1(2), sequence_path1(j)); % then sum the current processing time with this max valuue
+        queque_machine5(j,1)=completion_times_path1(2, j);
+        queque_machine5(j,2)=processing_times(machines_path1(3), sequence_path1(j));
     end
 
+
+    % repeat for path2
+
+       % variable initialization
+    
+    completion_times_path2 = zeros(3, num_jobs_path2); % matrix which will be used to keep track of jobs' completion times 
+
+    % Fill the completion times for the first job
+    completion_times_path2(1, 1) = processing_times(machines_path2(1), sequence_path2(1)); % set completion time of the first job on the first machine
+    % for each subsequent machine sum processing times, accumulating completion times
+    
+    completion_times_path2(2, 1) = completion_times_path2(1, 1) + processing_times(machines_path2(2), sequence_path2(1));
+    
+    queque_machine5(num_jobs_path1+1,1)=completion_times_path2(2, 1);
+    queque_machine5(num_jobs_path1+1,2)=processing_times(machines_path2(3), sequence_path2(1));
+
+    % Fill the completion times for the rest of the jobs
+    for j = 2:num_jobs_path2
+        % compute completion time on the first machine as the sum of completion time of the previous job and the current processing time
+        completion_times_path2(1, j) = completion_times_path2(1, j-1) + processing_times(machines_path2(1), sequence_path2(j));
+        % for each subsequent machine the completion time is the max between the completion time of the previous job on the same machine and the completion time of the current job on the previous machine
+        
+        completion_times_path2(2, j) = max(completion_times_path2(1, j), completion_times_path2(2, j-1)) + processing_times(machines_path2(2), sequence_path2(j)); % then sum the current processing time with this max valuue
+        queque_machine5(num_jobs_path1+j,1)=completion_times_path2(2, j);
+        queque_machine5(num_jobs_path1+j,2)=processing_times(machines_path2(3), sequence_path2(j));
+    end
+
+
+    queque_machine5=sortrows(queque_machine5,1);
+    total_time=queque_machine5(1,1)+queque_machine5(1,2);
+    for j = 2:(num_jobs_path1+num_jobs_path2)
+        total_time=max(queque_machine5(j,1),total_time)+queque_machine5(j,2);
+    end
     % Makespan is the last element in the completion times matrix
-    makespan = completion_times(end, end);
+    makespan = total_time;
 end
